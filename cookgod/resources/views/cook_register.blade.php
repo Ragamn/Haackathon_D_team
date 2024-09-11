ad83ccf61418d86e92e93b24574dab0aeff05586
OnoderaYuuma <y.onodera.sys23@morijyobi.ac.jp>
2024-09-11 12:26:33 +0900

    cook_registerの編集とcss,jsファイルの追加

--- cookgod/resources/views/cook_register.blade.php
+++ cookgod/resources/views/cook_register.blade.php
@@ -4,8 +4,23 @@
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <link rel="stylesheet" href="{{ asset('css/cook_register.css') }}">
 </head>
 <body>
+  <img src="" alt="">
   <h1>料理登録</h1>
+  <form action="">
+  <p>メニュー名</p>
+    <div class="input_file">
+      <div class="preview">
+        <input accept="image/*" id="imgFile" type="file">
+      </div>
+      <p class="btn_upload">
+        画像ファイルを選択してアップロード
+      </p>
+    </div>
+    <input type="submit">
+  </form>
+  <script src="../js/cook_register.js"></script>
 </body>
 </html>
