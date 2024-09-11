49ad9445f385cf168b3002019bdf5e8b3e949280
OnoderaYuuma <y.onodera.sys23@morijyobi.ac.jp>
2024-09-11 15:00:34 +0900

    料理登録ページの編集

--- cookgod/resources/views/cook_register.blade.php
+++ cookgod/resources/views/cook_register.blade.php
@@ -7,20 +7,37 @@
   <link rel="stylesheet" href="{{ asset('css/cook_register.css') }}">
 </head>
 <body>
-  <img src="" alt="">
-  <h1>料理登録</h1>
+  <img src="../../img/cookgod_logo.png" alt="cookgod">
+  <p>料理登録</p>
   <form action="">
-  <p>メニュー名</p>
-    <div class="input_file">
-      <div class="preview">
-        <input accept="image/*" id="imgFile" type="file">
-      </div>
-      <p class="btn_upload">
-        画像ファイルを選択してアップロード
-      </p>
+    メニュー名<br>
+    <input type="text">
+
+    <!-- フォームで選択した画像 -->
+    <img id="img-preview" accept="image/*" style="max-width: 300px; max-height: 300px; display: none;">
+
+    <div class="buttons">
+      <!-- フォーム -->
+      <input type="file" name="logo" id="file-input" accept=".jpg, .jpeg, .png, .gif">
     </div>
-    <input type="submit">
+    <input type="submit" id="submit">
   </form>
-  <script src="../js/cook_register.js"></script>
+
+  <!-- 画像プレビューjs -->
+  <script>
+    document.getElementById('file-input').addEventListener('change', function(event) {
+      const file = event.target.files[0];
+      if (file) {
+        const reader = new FileReader();
+        reader.onload = function(e) {
+          const imgPreview = document.getElementById('img-preview');
+          imgPreview.src = e.target.result;
+          imgPreview.style.display = 'block';
+        };
+        reader.readAsDataURL(file);
+      }
+    });
+  </script>
+
 </body>
 </html>
