69ff74ba1ae19210071be394d0a9a942704a4340
kaji <k.notsukitaira.sys23@morijyobi.ac.jp>
2024-09-12 10:40:27 +0900

    登録処理作成

--- cookgod/app/Http/Controllers/CookController.php
+++ cookgod/app/Http/Controllers/CookController.php
@@ -11,7 +11,27 @@ class CookController extends Controller
         var_dump($cooks);
         return view('index',compact('cooks'));
     }
-    public function cookregister() {
+    public function cookregister(Request $request) {
+        // バリデーションルールの設定
+        $validate_rule = [
+            'name' => 'required',
+            'img_pass' => 'required',
+        ];
+
+        // リクエストデータのバリデーション
+        $this->validate($request, $validate_rule);
         
+        // 新しいCooksモデルのインスタンスを作成
+        $cook = new Cooks();
+        
+        // フォームから送信されたデータをモデルに設定
+        $cook->name = $request->input('name');
+        $cook->img_pass = $request->input('img_pass');
+        
+        // データベースに保存
+        $cook->save();
+        
+        // ビューを返し、成功メッセージを表示
+        return view('hello.index', ['msg' => '正しく登録されました！']);
     }
 }
