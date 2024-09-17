4920641dbefc1c6bf873262809e9cadccbb8408c
kaji <k.notsukitaira.sys23@morijyobi.ac.jp>
2024-09-17 10:56:27 +0900

    headerの追加

---
+++ cookgod/resources/views/layouts/header.blade.php
@@ -0,0 +1,18 @@
+<header id="header">
+      <div class="navtext-container">
+        <div class="navtext"></div>
+        <a class="img"><img src="{{asset('img/header.png')}}" alt="" /></a>
+      </div>
+      <div>
+        <input type="checkbox" class="menu-btn" id="menu-btn" />
+        <label for="menu-btn" class="menu-icon"
+          ><span class="navicon"></span
+        ></label>
+        <ul class="menu">
+          <li class="top"><a class="menu_text" href="#home">home</a></li>
+          <li><a class="menu_text" href="#skills">skills</a></li>
+          <li><a class="menu_text" href="#projects">projects</a></li>
+          <li><a class="menu_text" href="#contact">contact</a></li>
+        </ul>
+      </div>
+</header>
