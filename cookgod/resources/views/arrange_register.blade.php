41d54d579ed3d9baad7b070e95134ff67793f6a3
Tahono08 <h.tashiro.sys23@morijyobi.ac.jp>
2024-09-20 09:36:28 +0900

    フォントを変更した

--- cookgod/resources/views/arrange_register.blade.php
+++ cookgod/resources/views/arrange_register.blade.php
@@ -5,6 +5,9 @@
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>アレンジ画面</title>
   <link rel="stylesheet" href="{{ asset('css/arrange_register.css') }}">
+  <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Merriweather:ital,wght@0,300;0,400;0,700;0,900;1,300;1,400;1,700;1,900&display=swap" rel="stylesheet">
 </head>
 <body>
   
